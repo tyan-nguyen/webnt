db437256cf498219df5228375b7ce3b83892663e
tyan-nguyen <nguyenvantyan@gmail.com>
2024-03-07 23:40:23 +0700

    update

--- themes/zero/layouts/guest/why.php
+++ themes/zero/layouts/guest/why.php
@@ -18,7 +18,7 @@
                     <img loading="lazy" src="/ntweb/images/icon-image/service-icon1.png" alt="service-icon">
                 </div>
                 <div class="ts-service-info">
-                    <h3 class="service-box-title"><a href="#">Kinh nghiệm</a></h3>
+                    <h3 class="service-box-title">Kinh nghiệm</h3>
                     <p>Với đội ngũ kỹ sư giàu kinh nghiệm và nhiều năm thi công, sản xuất và kinh doanh các lĩnh vực sẽ mang đến sự an tâm cho Khách hàng.</p>
                     <!-- <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Learn more</a> -->
                 </div>
@@ -36,7 +36,7 @@
                     <img loading="lazy" src="/ntweb/images/icon-image/service-icon2.png" alt="service-icon">
                 </div>
                 <div class="ts-service-info">
-                    <h3 class="service-box-title"><a href="#">Giá thành cạnh tranh</a></h3>
+                    <h3 class="service-box-title">Giá thành cạnh tranh</h3>
                     <p>Chúng tôi thực hiện từ khâu thiết kế, thi công, cung ứng vật tư, hạn chế tối đa vận hành qua nhiều trung gian mang đến giá thành cạnh tranh.</p>
                     <!-- <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Learn more</a> -->
                 </div>
@@ -54,7 +54,7 @@
                     <img loading="lazy" src="/ntweb/images/icon-image/service-icon3.png" alt="service-icon">
                 </div>
                 <div class="ts-service-info">
-                    <h3 class="service-box-title"><a href="#"></a>Cam kết về chất lượng</h3>
+                    <h3 class="service-box-title">Cam kết về chất lượng</h3>
                     <p>Với uy tín nhiều năm trong các ngành nghề hoạt động, chúng tôi cam kết về chất lượng sản phẩm đầu ra và chế độ bảo hành tốt nhất.</p>
                     <!-- <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Learn more</a> -->
                 </div>
