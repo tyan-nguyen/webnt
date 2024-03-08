48792cf31857b962b916548e7ee50ff965cb0341
tyan-nguyen <nguyenvantyan@gmail.com>
2024-03-08 16:25:39 +0700

    update

--- themes/zero/layouts/guest/footer.php
+++ themes/zero/layouts/guest/footer.php
@@ -109,7 +109,7 @@ $setting = Options::getOptions('SETTING');
     </a>
     
      <a id="call" href="tel:<?= $setting['site_hotline'] ?>">
-          <img src="/images/icons/call.png" width="35px" height="" style="padding-bottom: 2px">
+          <img class="trin-trin" src="/images/icons/call.png" width="35px" height="" style="padding-bottom: 2px">
     </a>
 
   </div><!-- Container end -->
