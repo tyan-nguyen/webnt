871a8e525c6e4b8e06e08ec73c12e5ed492ca5a4
Annvt <nguyenvantyan@gmail.com>
2024-03-09 10:34:31 +0700

    update

--- themes/zero/layouts/guest/googleAnalytic.php
+++ themes/zero/layouts/guest/googleAnalytic.php
@@ -1,1 +1,9 @@
-fasdfasf
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RFYFNX0CKD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RFYFNX0CKD');
+</script>
