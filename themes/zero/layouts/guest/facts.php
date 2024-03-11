d7a91fc34c8da8e911012b8059494f2fdc0eded3
Nvtan <nguyenvantyan@gmail.com>
2024-03-11 22:02:52 +0700

    update

--- themes/zero/layouts/guest/facts.php
+++ themes/zero/layouts/guest/facts.php
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-md-3 col-sm-6 ts-facts">
               <div class="ts-facts-img">
-                <img loading="lazy" src="/ntweb/images/icon-image/fact1.png" alt="facts-img">
+                <img loading="lazy" src="/ntweb/images/icon-image/fact1.png?v=1" alt="facts-img">
               </div>
               <div class="ts-facts-content">
                 <h2 class="ts-facts-num"><span class="counterUp" data-count="20">0</span></h2>
@@ -15,7 +15,7 @@
 
           <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-sm-0">
               <div class="ts-facts-img">
-                <img loading="lazy" src="/ntweb/images/icon-image/fact2.png" alt="facts-img">
+                <img loading="lazy" src="/ntweb/images/icon-image/fact2.png?v=1" alt="facts-img">
               </div>
               <div class="ts-facts-content">
                 <h2 class="ts-facts-num"><span class="counterUp" data-count="446">0</span>+</h2>
@@ -25,7 +25,7 @@
 
           <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
               <div class="ts-facts-img">
-                <img loading="lazy" src="/ntweb/images/icon-image/fact3.png" alt="facts-img">
+                <img loading="lazy" src="/ntweb/images/icon-image/fact3.png?v=1" alt="facts-img">
               </div>
               <div class="ts-facts-content">
                 <h2 class="ts-facts-num"><span class="counterUp" data-count="46">0</span>+</h2>
@@ -35,7 +35,7 @@
 
           <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
               <div class="ts-facts-img">
-                <img loading="lazy" src="/ntweb/images/icon-image/fact4.png" alt="facts-img">
+                <img loading="lazy" src="/ntweb/images/icon-image/fact4.png?v=1" alt="facts-img">
               </div>
               <div class="ts-facts-content">
                 <h2 class="ts-facts-num"><span class="counterUp" data-count="400">0</span>+</h2>
